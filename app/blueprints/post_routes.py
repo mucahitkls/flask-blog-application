4dccb474a8cd97f40ae9b64d657bebb0176d6526
mücahit keleş <mchtttkls@gmail.com>
2023-12-25 20:50:41 +0100

    error handling defined for routes

--- app/blueprints/post_routes.py
+++ app/blueprints/post_routes.py
@@ -27,18 +27,21 @@ def get_all_posts():
 def show_post(post_id):
     requested_post = crud_post.get_post_by_id(db=db, post_id=post_id)
     comment_form = CommentForm()
+    try:
+        if comment_form.validate_on_submit():
+            if not current_user.is_authenticated:
+                return redirect(url_for("user_routes.login"))
 
-    if comment_form.validate_on_submit():
-        if not current_user.is_authenticated:
-            return redirect(url_for("user_routes.login"))
+            result = crud_comment.get_comment_by_text(db=db, comment_form=comment_form)
+            if not result:
+                crud_comment.create_comment(db=db, comment_form=comment_form, requested_post=requested_post)
+                comment_form.comment_text.data = ""
+            elif result.comment_author != current_user:
+                crud_comment.create_comment(db=db, comment_form=comment_form, requested_post=requested_post)
+                comment_form.comment_text.data = ""
 
-        result = crud_comment.get_comment_by_text(db=db, comment_form=comment_form)
-        if not result:
-            crud_comment.create_comment(db=db, comment_form=comment_form, requested_post=requested_post)
-            comment_form.comment_text.data = ""
-        elif result.comment_author != current_user:
-            crud_comment.create_comment(db=db, comment_form=comment_form, requested_post=requested_post)
-            comment_form.comment_text.data = ""
+    except Exception as e:
+        logging.error(f"Showing post error for post_id: {post_id}, error: {e}")
     return render_template("post.html", post=requested_post, current_user=current_user, form=comment_form)
 
 
@@ -46,20 +49,22 @@ def show_post(post_id):
 @admin_only
 def add_new_post():
     form = CreatePostForm()
-
-    if form.validate_on_submit():
-        post_title = form.title.data
-        is_post_exists = crud_post.get_post_by_title(db=db, title=post_title)
-        if not is_post_exists:
-            new_post = crud_post.create_new_post(db=db, create_post_form=form)
-            logging.info(f"New post {form.title.data} created by {current_user.email}")
-            return redirect(url_for("post_routes.show_post", post_id=new_post.id))
-        else:
-            flash("There is already a post with that title", 'error')
-            logging.warning(f"Post title {form.title.data} already exists.")
-            form.title.data = ""
-            # Instead of redirecting, re-render the same page with the form containing the existing data
-            return render_template("make-post.html", form=form, type='create')
+    try:
+        if form.validate_on_submit():
+            post_title = form.title.data
+            is_post_exists = crud_post.get_post_by_title(db=db, title=post_title)
+            if not is_post_exists:
+                new_post = crud_post.create_new_post(db=db, create_post_form=form)
+                logging.info(f"New post {form.title.data} created by {current_user.email}")
+                return redirect(url_for("post_routes.show_post", post_id=new_post.id))
+            else:
+                flash("There is already a post with that title", 'error')
+                logging.warning(f"Post title {form.title.data} already exists.")
+                form.title.data = ""
+                # Instead of redirecting, re-render the same page with the form containing the existing data
+                return render_template("make-post.html", form=form, type='create')
+    except Exception as e:
+        logging.error(f"Post creation failed: {e}")
 
     return render_template("make-post.html", form=form, type='create')
 
@@ -75,15 +80,22 @@ def edit_post(post_id):
         img_url=blog_post.img_url,
         body=blog_post.body
     )
-    if post_to_edit_form.validate_on_submit():
-        updated_blog_post = crud_post.update_post(db=db, update_form=post_to_edit_form, post_to_update=blog_post)
-        if updated_blog_post:
-            return redirect(url_for("post_routes.show_post", post_id=blog_post.id))
+    try:
+        if post_to_edit_form.validate_on_submit():
+            updated_blog_post = crud_post.update_post(db=db, update_form=post_to_edit_form, post_to_update=blog_post)
+            if updated_blog_post:
+                return redirect(url_for("post_routes.show_post", post_id=blog_post.id))
+
+    except Exception as e:
+        logging.error(f"Post update failed: {e}")
     return render_template("make-post.html", form=post_to_edit_form, type='edit')
 
 
 @post_routes.route('/delete/<int:post_id>')
 @admin_only
 def delete_post(post_id):
-    crud_post.delete_post(db=db, post_id=post_id)
+    try:
+        crud_post.delete_post(db=db, post_id=post_id)
+    except Exception as e:
+        logging.error(f"Post deletion failed: {e}")
     return redirect(url_for("post_routes.get_all_posts"))
--- app/blueprints/user_routes.py
+++ app/blueprints/user_routes.py
@@ -12,49 +12,59 @@ user_routes = Blueprint('user_routes', __name__)
 @user_routes.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginUserForm()
+    try:
+        if login_form.validate_on_submit():
+            user = crud_user.get_user_by_mail(db=db, email=login_form.email.data)
+            password = login_form.password.data
+            if not user:
+                flash("User does not exist")
+                logging.warning("Failed login attempt - User does not exist.")
+                return redirect(url_for("user_routes.login"))
 
-    if login_form.validate_on_submit():
+            elif not authentication.confirm_password(user.password, password):
+                flash("email or password is wrong")
+                logging.warning("Failed login attempt - email or password wrong.")
+                return redirect(url_for("user_routes.login"))
 
-        user = crud_user.get_user_by_mail(db=db, email=login_form.email.data)
-        password = login_form.password.data
-        if not user:
-            flash("User does not exist")
-            logging.warning("Failed login attempt - User does not exist.")
-            return redirect(url_for("user_routes.login"))
-
-        elif not authentication.confirm_password(user.password, password):
-            flash("email or password is wrong")
-            logging.warning("Failed login attempt - email or password wrong.")
-            return redirect(url_for("user_routes.login"))
-        else:
             logging.info(f"User {user.email} logged in successfully.")
             login_user(user)
             return redirect(url_for("post_routes.get_all_posts"))
 
+    except Exception as e:
+        logging.error(f"Login error: {e}")
+        flash("An unexpected error occurred while logging in.", "error")
     return render_template("login.html", form=login_form, current_user=current_user)
 
 
 @user_routes.route("/register", methods=["GET", "POST"])
 def register():
     register_form = RegisterUserForm()
+    try:
+        if register_form.validate_on_submit():
+            user = crud_user.get_user_by_mail(db=db, email=register_form.email.data)
+            if user:
+                flash("user already exists, please try to login")
+                logging.info(f"User {user.email} already exists.")
+                return redirect(url_for('user_routes.login'))
 
-    if register_form.validate_on_submit():
-        user = crud_user.get_user_by_mail(db=db, email=register_form.email.data)
-        if user:
-            flash("user already exists, please try to login")
-            logging.info(f"User {user.email} already exists.")
-            return redirect(url_for('user_routes.login'))
+            user_to_register = crud_user.create_user(db=db, register_form=register_form)
+            if user_to_register:
+                login_user(user_to_register)
+                logging.warning("Registration failed.")
+                return redirect(url_for("post_routes.get_all_posts"))
 
-        user_to_register = crud_user.create_user(db=db, register_form=register_form)
-        if user_to_register:
-            login_user(user_to_register)
-            logging.warning("Registration failed.")
-            return redirect(url_for("post_routes.get_all_posts"))
+    except Exception as e:
+        logging.error(f"Registration error: {e}")
+        flash("An unexpected error occurred during registration.", "error")
     return render_template("register.html", form=register_form, current_user=current_user)
 
 
 @user_routes.route("/logout")
 def logout():
-    logout_user()
-    logging.info(f"User {current_user.email} logged out.")
+    try:
+        logout_user()
+        logging.info(f"User {current_user.email} logged out.")
+    except Exception as e:
+        logging.error(f"Logout error: {e}")
+        flash("An unexpected error occurred during registration.", "error")
     return redirect(url_for("post_routes.get_all_posts"))
